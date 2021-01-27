f0c7536a0341c4f70c8d841da0027a339f9a02ae
Carlos <cadudrop@hotmail.com>
2021-01-27 11:53:43 -0300

    accordion complete

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const accordion = document.getElementsByClassName("contentBx");
+const accordion = document.getElementsByClassName("label");
+const panel = document.getElementsByClassName("content");
 
 for(let i=0; i<accordion.length; i++){
     accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
+        var setClasses = !this.classList.contains('active');
+        setClass(accordion, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
     })
 }
 
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
+}
